e3d319029da7f6a3c8ee55fa4cb75e3730fac03e
Muskan <113929010+Muskan1009@users.noreply.github.com>
2024-03-03 08:06:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+        purple: "rgba(114, 65, 252, 1)",
+        purpleG: "rgba(114, 65, 252, 1)66.94%",
+        whiteG: "rgba(225, 215, 255, 1)106.57%",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
+
